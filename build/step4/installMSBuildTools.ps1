1862bafc4ddddcadff52cb5cb3ed7bc619b0a7ca
Dag H. Baardsen <dagh@baardsen.org>
2017-09-14 15:06:17 +0200

    Fixed MS Build Tools install wait logic

--- build/step4/installMSBuildTools.ps1
+++ build/step4/installMSBuildTools.ps1
@@ -5,12 +5,26 @@ Write-Host 'Installing Build Tools'
 ### Build Tools component ids only: https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-build-tools
 /install/vs_BuildTools.exe --quiet --norestart --includeRecommended --add Microsoft.VisualStudio.Workload.MSBuildTools --add Microsoft.VisualStudio.Workload.NetCoreBuildTools --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Workload.WebBuildTools --add Microsoft.Net.Component.4.6.2.SDK --add Microsoft.Net.Component.4.6.2.TargetingPack --add Microsoft.Net.Component.4.7.SDK --add Microsoft.Net.Component.4.7.TargetingPack --add Microsoft.Net.Component.4.5.1.TargetingPack
 
-Write-Host 'Waiting for Build Tools background setup to start'
-Start-Sleep -s 180
-
 $loopit = $true
 
-### Wait for installation sto end. Poll every 10 seconds
+Write-Host 'Waiting for Installer to start'
+while ($loopit)
+{
+    if ((Get-Process "vs_installer" -ErrorAction SilentlyContinue) -eq $null)
+    {
+        Write-Host '#' -NoNewline
+        Start-Sleep -s 10
+    }
+    else
+    {
+        $loopit = $false
+    }
+}
+
+Write-Host ''
+Write-Host 'Waiting for Installer to end'
+$loopit = $true
+
 while ($loopit)
 {
     if ((Get-Process "vs_installer" -ErrorAction SilentlyContinue) -ne $null)
